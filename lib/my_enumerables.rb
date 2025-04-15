d334c1be8364579f59405d362a352b3cd1840c18
Sadaf Al Arabi <sadafalarabi@mac.lan>
2025-04-15 00:08:46 -0400

    Solved the my_select method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,23 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    if block_given?
+      for i in 0...self.length
+        yield(self[i], i)
+      end
+    end
+    self
+  end
+
+  def my_select
+    a = []
+    if block_given?
+      for i in 0...self.length
+        a << self[i] if yield(self[i])
+      end
+    end
+    a
+  end
 end
 
 # You will first have to define my_each
